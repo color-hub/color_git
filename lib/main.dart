6d89d4cd0c00e2c6513bb257c94f7f38a1fe234d
Luka Katsadze <suxoikorm@gmail.com>
2022-11-19 00:42:50 +0400

    Libgit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:libgit2dart/libgit2dart.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,30 +22,27 @@ class MyApp extends StatelessWidget {
 
 class Home extends StatelessWidget {
   const Home({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TextButton(
         child: const Text('test'),
-        onPressed: () async {
-          final XTypeGroup typeGroup = XTypeGroup(
-            label: 'images',
-            extensions: <String>['jpg', 'png'],
-          );
-          final XFile? file = await openFile(acceptedTypeGroups: <XTypeGroup>[typeGroup]);
-          print(file?.path);
-          // String? path = await FilesystemPicker.open(
-          //   title: 'Save to folder',
-          //   context: context,
-          //   rootDirectory: Directory.current,
-          //   fsType: FilesystemType.folder,
-          //   pickText: 'Save file to this folder',
-          //   folderIconColor: Colors.teal,
-          // );
-          // print(path.toString());
+        onPressed: () {
+          print(Libgit2.version);
+          //final repo = Repository.open('D:Repo/friflex/bristol_mobile');
+          // log('test');
+          // final String? directoryPath = await getDirectoryPath();
+          // if (directoryPath != null) {
+          //   log(directoryPath.toString());
+          //   try {
+          //     print(repo.branches);
+          //   } catch (e) {
+          //     log(e.toString());
+          //   }
+          // }
         },
       ),
     );
